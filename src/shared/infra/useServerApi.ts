1ebf2e4adcd4b9eef3b3dde6aefea3af65080eed
Dayem Siddiqui <dayemsiddiqui@gmail.com>
2021-08-24 20:24:14 +0200

    Timeout exceeded

--- src/shared/infra/useServerApi.ts
+++ src/shared/infra/useServerApi.ts
@@ -1,7 +1,6 @@
 import { DefaultApiFactory } from "smart-youtuber-client-sdk/api/api";
 import axios from "axios";
 export const useServerApi = () => {
-  // Update the backend url to be on gcloud
   if (!process.env.REACT_APP_SERVER_BASE_URL) {
     throw new Error("Server Base URL undefined");
   }
@@ -9,7 +8,6 @@ export const useServerApi = () => {
 
   const apiClient = axios.create({
     baseURL: baseURL,
-    timeout: 1000,
   });
   const { channelList, spamList, videoList } = DefaultApiFactory(
     {
