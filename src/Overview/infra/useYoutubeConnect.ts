6a863e0d6afcc3413f9fb39ceb6fad92ff022d26
Dayem Siddiqui <dayemsiddiqui@gmail.com>
2021-08-25 00:57:26 +0200

    added the auth token header

--- src/Overview/infra/useYoutubeConnect.ts
+++ src/Overview/infra/useYoutubeConnect.ts
@@ -1,9 +1,15 @@
 import { useServerApi } from "../../shared/infra/useServerApi";
+import { auth } from "../../Authentication";
 
 export const useYoutubeConnect = () => {
   const { apiClient } = useServerApi();
   const connect = async () => {
-    const response = await apiClient.get("auth/connect");
+    const idToken = await auth().currentUser?.getIdToken(true);
+    const response = await apiClient.get("auth/connect", {
+      headers: {
+        "x-id-token": idToken,
+      },
+    });
     const url = response.data.url;
     window.location.replace(url);
     console.log({ response });
