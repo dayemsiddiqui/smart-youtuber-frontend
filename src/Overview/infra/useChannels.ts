5499c8ff00eeb97a719b3d1e0dfc7d02d92a2b69
Dayem Siddiqui <dayemsiddiqui@gmail.com>
2021-08-26 15:49:00 +0200

    updated the frontend

--- src/Overview/infra/useChannels.ts
+++ src/Overview/infra/useChannels.ts
@@ -3,10 +3,18 @@ import { useLoadDataAsync } from "../../shared/infra/useLoadDataAsync";
 
 export const useChannels = () => {
   const { channelList } = useServerApi();
-  const { data, isLoading } = useLoadDataAsync(channelList, []);
+  const { data, isLoading } = useLoadDataAsync(channelList, {
+    items: [],
+    nextPageToken: "",
+    prevPageToken: "",
+    pageInfo: {
+      totalResults: 0,
+      resultsPerPage: 0,
+    },
+  });
 
   return {
-    channels: data,
+    channels: data.items,
     isLoading,
   };
 };
